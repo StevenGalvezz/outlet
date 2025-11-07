acedb5e2c2642d992f7afe9fcbec154fee900c25
xsf <xsf@DESKTOP-5GJCLST>
2025-11-07 00:34:28 -0600

    index

--- src/main/java/com/outlet/domain/Categoria.java
+++ src/main/java/com/outlet/domain/Categoria.java
@@ -24,6 +24,7 @@ public class Categoria implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
     private Integer id_categoria;
 
     @NotNull
